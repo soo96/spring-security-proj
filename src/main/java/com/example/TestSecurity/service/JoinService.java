3af184f84994ab189653b0500adb122a9a1b684d
soo <dlgustn96@gmail.com>
2025-09-29 01:45:59 +0900

    feat: 회원가입 및 중복검증 구현

--- src/main/java/com/example/TestSecurity/service/JoinService.java
+++ src/main/java/com/example/TestSecurity/service/JoinService.java
@@ -17,12 +17,17 @@ public class JoinService {
     public void joinProccess(JoinDTO joinDTO) {
 
         //db에 이미 동일한 username을 가진 회원이 존재하는지?
+        boolean isUser = userRepository.existsByUsername(joinDTO.getUsername());
+
+        if (isUser) {
+            return;
+        }
 
         UserEntity user = new UserEntity();
 
         user.setUsername(joinDTO.getUsername());
         user.setPassword(bCryptPasswordEncoder.encode(joinDTO.getPassword()));
-        user.setRole("ROLE_USER");
+        user.setRole("ROLE_ADMIN");
 
         userRepository.save(user);
     }
